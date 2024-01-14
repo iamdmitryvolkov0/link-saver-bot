e77b87cd938032f2a2f0d6294f2eb4666cda944b
DmitryVolkov <iamdmitryvolkov@gmail.com>
2024-01-14 17:41:44 +0700

    request methods added

---
+++ clients/telegram/telegram.go
@@ -0,0 +1,62 @@
+package telegram
+
+import (
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+
+	"bot/lib/e"
+)
+
+type Client struct {
+	host     string
+	basePath string
+	client   http.Client
+}
+
+func New(host string, token string) Client {
+	return Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
+
+func (c *Client) Update(offset int, limit int) ([]Update, error) {
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+}
+
+func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+	const errMsg = "cant do request"
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+
+	if err != nil {
+		return nil, e.Wrap(errMsg, err)
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, e.Wrap(errMsg, err)
+
+	}
+}
+
+func (c *Client) SendMessage() {
+
+}
--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package bot
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func main() {
+	t := mustToken()
+	fmt.Println(t)
+	//tgClient := telegram.New(token)
+
+	//fetcher := fetcher.New(tgClient)
+
+	//processor := processor.New(tgClient)
+
+	//consumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	t := flag.String(
+		"bot-token",
+		"",
+		"Token to access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *t == "" {
+		log.Fatal("token is empty")
+	}
+
+	return t
+}
